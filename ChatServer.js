74c4c112e04da0ba1ea7671f41ae619ca959dd0e
user32dll <116982494+cryptographer3301@users.noreply.github.com>
2022-12-26 15:36:29 +0100

    Update and rename chat.js to ChatServer.js

--- ChatServer.js
+++ ChatServer.js
@@ -5,12 +5,15 @@ const sockets = []; // empty array to collect users .
 const HTTP = net.Server(function(socket,  ...args) {
     sockets.push(socket);
 
+    // Send message
     socket.on('data', function(message){
         for (var i = 0; i <= sockets.length; i++){
-            sockets[i].write(message);
+            if (sockets[i] == socket) continue;
+            sockets[i].write(message); // Pseudo
         }
     });
 
+    // Receive message
     socket.on('end', function() {
         let i = sockets.indexOf(socket);
         sockets.splice(i, 1);
